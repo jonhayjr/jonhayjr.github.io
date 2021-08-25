05db635306276345073850ed6012732e9bd83d43
jonhayjr <jonhayjr@gmail.com>
2021-08-24 17:27:30 -0700

    Updated code to use Mongo DB and Mongo DB Realm servless backend

---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+  const Config = {
+    //Store base url for api
+    apiProjectsUrl: 'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/portfolio-app-nhcur/service/projects/incoming_webhook/projects',
+    apiProjectSkillUrl: 'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/portfolio-app-nhcur/service/project/incoming_webhook/project'
+ }
+
+ export default Config;
